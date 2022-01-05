f455463d52fb345b70bbe126a906d8caaead331c
mattlockyer <mattdlockyer@gmail.com>
2022-01-05 07:38:30 -0800

    add react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,4 +1,9 @@
 import React, { useContext, useEffect } from 'react';
+import {
+	Routes,
+	Route,
+	Link
+} from "react-router-dom";
 
 import { appStore, onAppMount } from './state/app';
 
@@ -21,10 +26,31 @@ const App = () => {
 	};
 
 	return (
-		<div className="root">
-			<HelloMessage message={state.foo && state.foo.bar.hello} />
-			<p>clicked: {JSON.stringify(state.clicked)}</p>
-			<button onClick={handleClick}>Click Me</button>
+		<div>
+
+			<nav>
+				<ul>
+					<li>
+						<Link to="/">Home</Link>
+					</li>
+					<li>
+						<Link to="/hello">Hello</Link>
+					</li>
+				</ul>
+			</nav>
+
+			<Routes>
+				<Route path="/hello" element={
+					<HelloMessage message={state.foo && state.foo.bar.hello} />
+				} />
+				<Route path="/" element={
+					<>
+					<p>clicked: {JSON.stringify(state.clicked)}</p>
+					<button onClick={handleClick}>Click Me</button>
+					</>
+				} />
+			</Routes>
+
 		</div>
 	);
 };
--- src/index.js
+++ src/index.js
@@ -2,10 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import { AppProvider } from './state/app.js';
+import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.render(
 	<AppProvider>
-		<App />
+		<BrowserRouter>
+			<App />
+		</BrowserRouter>
 	</AppProvider>,
 	document.getElementById('root')
 );
