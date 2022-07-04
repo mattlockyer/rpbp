2c25e006002a1b06f102d8fab51be0fb818d3f16
mattlockyer <mattdlockyer@gmail.com>
2022-07-04 06:56:27 -0700

    update near with more app style example

--- src/App.js
+++ src/App.js
@@ -2,69 +2,68 @@ import React, { useContext, useEffect } from 'react';
 import {
 	Routes,
 	Route,
-	Link
+	useLocation,
 } from "react-router-dom";
 
 import { appStore, onAppMount } from './state/app';
+import { Header } from './components/Header';
+import { Home } from './components/Home';
 
-import HelloMessage from './HelloMessage';
-
+import './css/normalize.css';
+import './css/skeleton.css';
 import './App.scss';
 
 const App = () => {
 	const { state, dispatch, update } = useContext(appStore);
 
-	console.log('state', state);
-
-	const { wallet, account } = state
+	const { app, wallet, account } = state
+	const { menu } = app
+	const { pathname } = useLocation();
 
 	const onMount = () => {
-		dispatch(onAppMount('world'));
+		dispatch(onAppMount());
 	};
 	useEffect(onMount, []);
 
-	const handleClick = () => {
-		update('clicked', !state.clicked);
-	};
+	const routeArgs = {
+		state, update
+	}
 
 	return (
 		<div>
-
-			<nav>
-				<ul>
-					<li>
-						<Link to="/">Home</Link>
-					</li>
-					<li>
-						<Link to="/hello">Hello</Link>
-					</li>
-					<li>
-						<Link to="/wallet">Wallet</Link>
-					</li>
-				</ul>
-			</nav>
-
-			<Routes>
-				<Route path="/wallet" element={
-					account ? <>
-						<p>{ account.accountId }</p>
-						<button onClick={() => wallet.signOut()}>Sign Out</button>
-					</> :
-					<>
-						<p>Not Signed In</p>
-						<button onClick={() => wallet.signIn()}>Sign In</button>
-					</>
-				} />
-				<Route path="/hello" element={
-					<HelloMessage message={state.foo && state.foo.bar.hello} />
-				} />
-				<Route path="/" element={
-					<>
-						<p>clicked: {JSON.stringify(state.clicked)}</p>
-						<button onClick={handleClick}>Click Me</button>
-					</>
-				} />
-			</Routes>
+			<Header {...{ pathname, menu, account, update }} />
+			{
+				account ?
+					/* Account Paths */
+					<main>
+						<Routes>
+							<Route path="/account" element={
+								<>
+									<p>{account.accountId}</p>
+									<button onClick={() => wallet.signOut()}>Sign Out</button>
+								</>
+							} />
+							<Route path="/" element={<Home {...routeArgs} />} />
+						</Routes>
+					</main>
+					:
+					/* Public Paths */
+					<main>
+						<Routes>
+							<Route path="/about" element={
+								<>
+									<p>App is dope</p>
+								</>
+							} />
+							<Route path="/" element={
+								<>
+									<p>Please sign in to get started</p>
+									<button onClick={() => wallet.signIn()}>Sign In</button>
+								</>
+							} />
+						</Routes>
+					</main>
+			}
 
 		</div>
 	);
---
+++ src/components/Header.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import {
+	Routes,
+	Route,
+	Link
+} from "react-router-dom";
+
+import './Header.scss';
+
+import { Menu } from 'react-feather';
+
+const Links = ({ update, account }) => {
+	const hideMenu = () => update('app.menu', false)
+	return <nav>
+		<Link onClick={hideMenu} to="/">Home</Link>
+		<Link onClick={hideMenu} to="/about">About</Link>
+		{
+			account && <>
+				<Link onClick={hideMenu} to="/account">Account</Link>
+			</>
+		}
+	</nav>
+}
+
+export const Header = ({ pathname, menu, account, update }) => {
+	return <header>
+		<div>
+			<p>
+				App { pathname.length > 1 && '/ ' + pathname.substring(1) }
+			</p>
+		</div>
+		<div>
+			<Menu onClick={() => update('app', { menu: !menu })} />
+			<Links {...{ update, account }} />
+		</div>
+		{menu && window.innerWidth < 768 && <Links {...{ update, account }} />}
+	</header>
+}
